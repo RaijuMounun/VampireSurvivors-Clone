36598464bfa7840cf020f2b45c934f73bfc41145
Eren Keskinoğlu <erenkeskinoglu@outlook.com>
2023-12-09 23:48:45 +0300

    Commit 1
    Basic Environment and Player
    Basic Movement
    Singleton Setup
    n sum shit

---
+++ Assets/Scripts/Business/Character.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    PlayerStats playerStats = PlayerStats.Instance;
+    public int Health { get; set; }
+    public int Damage { get; set; }
+    public bool IsAlive { get; set; }
+
+    private void Awake()
+    {
+        Health = playerStats.Health;
+        Damage = playerStats.Damage;
+
+    }
+    void Start()
+    {
+        IsAlive = true;
+    }
+
+
+    public void CheckDeath()
+    {
+        if (Health > 0) return;
+        IsAlive = false;
+        Death();
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+}
